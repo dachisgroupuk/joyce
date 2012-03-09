be8a6a86020c379d97a56eecce6c5f25e2f7bf00
Alessandro Morandi <alessandro@dachisgroup.com>
2012-03-09 17:36:18 +0000

    Better Rspec output.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,11 +7,12 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 
 require 'joyce'
 
+# Migrations
 require 'generators/joyce/templates/migration'
-
 ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :database => 'development.db')
 AddJoyceTables.up
 
+# Fixtures
 class Thing < ActiveRecord::Base
   
 end
@@ -19,4 +20,10 @@ ActiveRecord::Base.connection.execute("DROP TABLE IF EXISTS `things`")
 ActiveRecord::Base.connection.create_table(:things) do |t|
     t.string :name
     t.timestamps
+end
+
+# Prettyfying
+RSpec.configure do |config|
+  config.color_enabled  = true
+  config.formatter      = 'documentation'
 end
