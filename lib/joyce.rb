7b60329df96d25b0e5d779c8f5726e5d87571f37
Alessandro Morandi <alessandro@dachisgroup.com>
2012-03-11 14:30:14 +0000

    Added parameter validation to the Joyce.publish_activity method.

--- lib/joyce.rb
+++ lib/joyce.rb
@@ -3,6 +3,9 @@ require 'joyce/activity'
 
 module Joyce
   def self.publish_activity(args)
+    raise ArgumentError.new("An actor must be specified for the Activity") unless args[:actor]
+    raise ArgumentError.new("A verb must be specified for the Activity") unless args[:verb]
+    
     Activity.create(:actor => args[:actor], :verb => args[:verb], :obj => args[:obj])
   end
 end
--- spec/joyce_spec.rb
+++ spec/joyce_spec.rb
@@ -1,6 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Joyce do
+  
   describe ".publish_activity" do
     before do
       @actor = Thing.create(:name => "Ulysses")
@@ -8,14 +9,40 @@ describe Joyce do
       @object = Thing.create(:name => "Polyphemus")
     end
     
+    subject { Joyce.publish_activity(:actor => @actor, :verb => @verb, :obj => @object) }
+    
     it "should create an Activity" do
       expect{
-        Joyce.publish_activity(:actor => @actor, :verb => @verb, :obj => @object)
+        subject
       }.to change{ Joyce::Activity.count }.by(1)
+    end
+    
+    it "should return the activity" do
+      subject.should == Joyce::Activity.last
+    end
+    
+    it "should save the actor" do
+      subject.actor.should == @actor
+    end
+    
+    it "should save the verb" do
+      subject.verb.should == @verb
+    end
+    
+    it "should save the object" do
+      subject.obj.should == @object
+    end
+    
+    context "with missing parameters" do
+      [
+        {:verb => "fooled"},
+        {:actor => Thing.create(:name => "Ulysses")}
+      ].each do |params|
+        context "with #{params}" do
+          it{ expect{ Joyce.publish_activity(params) }.to raise_error ArgumentError }
+        end
+      end
     end
   end
   
-  it "should have a method" do
-    Joyce.methods.should include(:publish_activity)
-  end
 end
