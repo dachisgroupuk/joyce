28ef7238655336e0b19cf03d7e45b3e3de94e1f8
Alessandro Morandi <alessandro@dachisgroup.com>
2012-03-11 15:13:17 +0000

    Minor refactoring: removed sample spec.

--- spec/activity_spec.rb
+++ spec/activity_spec.rb
@@ -1,10 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Joyce::Activity do
-  it "should exist" do
-    Joyce::Activity.class.should_not be_nil
-  end
-  
   describe "validation" do
     context "with missing parameters" do
       [
