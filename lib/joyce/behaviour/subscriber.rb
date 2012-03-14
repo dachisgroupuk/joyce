2edf9b6ee50eab3c0756a68339b0c259bcd4dc2a
Alessandro Morandi <alessandro@dachisgroup.com>
2012-03-14 10:59:25 +0000

    Added method unsubscribe_from to Subscriber mixin. Added exceptions for trying to subscribe twice or trying to unsubscribe when not subscribed.

--- lib/joyce/behaviour/subscriber.rb
+++ lib/joyce/behaviour/subscriber.rb
@@ -1,23 +1,45 @@
 module Joyce
+  class NoSubscriptionError < RuntimeError; end
+  class DuplicateSubscriptionError < RuntimeError; end
+  
   module Behaviour
     
     module Subscriber
+      
       def self.included(base)
         base.has_many :stream_subscriptions, :class_name => 'Joyce::StreamSubscriber', :as => :subscriber
       end
       
       def subscribe_to(producer)
+        stream = stream_from_producer(producer)
+        raise DuplicateSubscriptionError.new("An open subscription to #{producer} already exists") unless stream_subscriptions.where(:stream_id => stream.id, :ended_at => nil).empty?
+        
+        Joyce::StreamSubscriber.create(:subscriber => self, :stream => stream, :started_at => Time.now)
+      end
+      
+      def unsubscribe_from(producer)
+        stream = stream_from_producer(producer)
+        stream_subscriber = stream_subscriptions.where(:stream_id => stream.id).first
+        raise NoSubscriptionError.new("No subscription to #{producer} could be found") if stream_subscriber.nil?
+
+        stream_subscriber.ended_at = Time.now
+        stream_subscriber.save
+      end
+
+
+      private
+
+      def stream_from_producer(producer)
         if producer.is_a?(Joyce::Stream)
-          Joyce::StreamSubscriber.create(:subscriber => self, :stream => producer, :started_at => Time.now)
+          stream = producer
         elsif producer.is_a?(Joyce::Behaviour::Owner)
-          Joyce::StreamSubscriber.create(:subscriber => self, :stream => Joyce::Stream.find_or_create_by_owner(producer), :started_at => Time.now)
+          stream = Joyce::Stream.find_or_create_by_owner(producer)
+        else
+          raise ArgumentError.new("Either a Stream or a stream Owner should be specified")
         end
       end
     end
     
-    def unsubscribe_from(producer)
-      
-    end
-    
   end
+  
 end
--- spec/behaviour/subscriber_spec.rb
+++ spec/behaviour/subscriber_spec.rb
@@ -7,47 +7,104 @@ describe Joyce::Behaviour::Subscriber do
   it { subscriber.stream_subscriptions.should be_empty }
   
   describe "#subscribe_to" do
-    shared_examples_for "a subscription creator" do
-      it "should add a subscription" do
-        expect{
-          subscriber.subscribe_to(@producer)
-        }.to change{ subscriber.stream_subscriptions.count }.by(1)
-      end
-      
-      it "should save started_at" do
-        time = Time.now
-        Timecop.freeze(time) { subscriber.subscribe_to(@producer) }
-        
-        subscriber.stream_subscriptions.last.started_at.should == time
-      end
-      
-      it "should not save ended_at" do
-        subscriber.subscribe_to(@producer)
-        subscriber.stream_subscriptions.last.ended_at.should be_nil
-      end
-    end
-    
-    context "with stream as a parameter" do
-      let(:stream) { Joyce::Stream.create(:owner => create(:thing)) }
-      before{ @producer = stream }
-      
-      it_should_behave_like "a subscription creator"
-    end
-    
-    context "with model instance as a parameter" do
-      let(:model) { create(:thing) }
-      
-      context "when model instance has got a stream" do
-        before{ @stream = Joyce::Stream.create(:owner => model) }
-        before{ @producer = model }
+    context "when not subscribed" do
+      shared_examples_for "a subscription creator" do
+        it "should add a subscription" do
+          expect{
+            subscriber.subscribe_to(@producer)
+          }.to change{ subscriber.stream_subscriptions.count }.by(1)
+        end
 
-        it_should_behave_like "a subscription creator"
-        
-        it "should save the model stream" do
-          subscriber.subscribe_to(model)
-          subscriber.stream_subscriptions.last.stream.should == @stream
+        it "should save started_at" do
+          time = Time.now
+          Timecop.freeze(time) { subscriber.subscribe_to(@producer) }
+
+          subscriber.stream_subscriptions.last.started_at.should == time
+        end
+
+        it "should not save ended_at" do
+          subscriber.subscribe_to(@producer)
+          subscriber.stream_subscriptions.last.ended_at.should be_nil
         end
       end
+
+      context "with stream as a parameter" do
+        let(:stream) { Joyce::Stream.create(:owner => create(:thing)) }
+        before{ @producer = stream }
+
+        it_should_behave_like "a subscription creator"
+      end
+
+      context "with model instance as a parameter" do
+        let(:model) { create(:thing) }
+
+        context "when model instance has got a stream" do
+          before{ @stream = Joyce::Stream.create(:owner => model) }
+          before{ @producer = model }
+
+          it_should_behave_like "a subscription creator"
+
+          it "should save the model stream" do
+            subscriber.subscribe_to(model)
+            subscriber.stream_subscriptions.last.stream.should == @stream
+          end
+        end
+      end
+
+      context "with wrong parameter" do
+        it{ expect{ subscriber.subscribe_to(Object.new) }.to raise_error ArgumentError }
+      end
+    end
+    
+    context "when subscribed" do
+      let(:model) { create(:thing) }
+      before do
+        @stream = Joyce::Stream.create(:owner => model)
+        subscriber.subscribe_to(@stream)
+      end
+      
+      it{ expect{ subscriber.subscribe_to(@stream) }.to raise_error Joyce::DuplicateSubscriptionError }
+    end
+  end
+  
+  describe "#unsubscribe_from" do
+    context "when subscribed" do
+      let(:model) { create(:thing) }
+      before do
+        @stream = Joyce::Stream.create(:owner => model)
+        subscriber.subscribe_to(@stream)
+      end
+      
+      context "with stream as a parameter" do
+        before{ @producer = @stream }
+
+        it "should save ended_at" do
+          time = Time.now
+          Timecop.freeze(time) { subscriber.unsubscribe_from(@producer) }
+
+          subscriber.stream_subscriptions.last.ended_at.should == time
+        end
+      end
+
+      context "with model instance as a parameter" do
+        before{ @producer = model }
+
+        it "should save ended_at" do
+          time = Time.now
+          Timecop.freeze(time) { subscriber.unsubscribe_from(@producer) }
+
+          subscriber.stream_subscriptions.last.ended_at.should == time
+        end
+      end
+
+      context "with wrong parameter" do
+        it{ expect{ subscriber.unsubscribe_from(Object.new) }.to raise_error ArgumentError }
+      end
+    end
+    
+    context "when not subscribed" do
+      let(:stream) { Joyce::Stream.create(:owner => create(:thing)) }
+      it{ expect{ subscriber.unsubscribe_from(stream) }.to raise_error Joyce::NoSubscriptionError }
     end
   end
   
