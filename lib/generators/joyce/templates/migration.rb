b25da0037df3248043e1c4d8844154a070b81ea3
Alessandro Morandi <alessandro@dachisgroup.com>
2012-03-19 14:54:45 +0000

    Added database indexes.

--- lib/generators/joyce/templates/migration.rb
+++ lib/generators/joyce/templates/migration.rb
@@ -6,17 +6,22 @@ class AddJoyceTables < ActiveRecord::Migration
       t.references  :obj, :polymorphic => true
       t.timestamps
     end
+    add_index(:joyce_activities, :created_at)
     
     create_table 'joyce_streams', :force => true do |t|
       t.string      'name'
       t.references  :owner, :polymorphic => true, :null => false
       t.timestamps
     end
+    add_index(:joyce_streams, :owner_type)
+    add_index(:joyce_streams, [:owner_id, :owner_type])
     
     create_table 'joyce_activities_streams', :id => false, :force => true do |t|
       t.references  :activity, :null => false
       t.references  :stream, :null => false
     end
+    add_index(:joyce_activities_streams, :activity_id)
+    add_index(:joyce_activities_streams, :stream_id)
     
     create_table 'joyce_activities_targets', :id => false, :force => true do |t|
       t.string      'name'
@@ -30,6 +35,8 @@ class AddJoyceTables < ActiveRecord::Migration
       t.datetime    'started_at', :null => false
       t.datetime    'ended_at'
     end
+    add_index(:joyce_streams_subscribers, [:subscriber_id, :subscriber_type], :name => "index_joyce_streams_subscribers_on_subscriber")
+    add_index(:joyce_streams_subscribers, :stream_id)
   end
 
   def self.down
